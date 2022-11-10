181fc6bb26717a4fab8c40aa354a102d2d879750
Radik Akhmetzianov <109820745+RadAkhme@users.noreply.github.com>
2022-11-10 20:49:58 +0300

    Initial Commit

---
+++ MarvelAPI Alamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelAPI Alamofire
+//
+//  Created by Радик Ахметзянов on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
